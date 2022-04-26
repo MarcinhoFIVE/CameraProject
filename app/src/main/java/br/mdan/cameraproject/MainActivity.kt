df5dc12dbf578e58536e66c9a91bb1bedb2686bd
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-26 12:42:38 -0300

    Permission and Shoot Button

--- app/src/main/java/br/mdan/cameraproject/MainActivity.kt
+++ app/src/main/java/br/mdan/cameraproject/MainActivity.kt
@@ -1,11 +1,113 @@
 package br.mdan.cameraproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val PERMISSION_CODE_IMAGE_PICK = 1000
+        private val IMAGE_PICK_CODE = 2000
+        private val PERMISSION_CODE_SHOOT_IMAGE = 3000
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        btnPick.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE_IMAGE_PICK)
+                } else {
+                    pickImageFromGalery()
+                }
+            } else {
+                pickImageFromGalery()
+            }
+        }
+
+        btnShoot.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permission = arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                    requestPermissions(permission, PERMISSION_CODE_SHOOT_IMAGE)
+                } else {
+                    openCamera()
+                }
+            } else {
+                openCamera()
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE_IMAGE_PICK -> {
+                if (grantResults.size > 0 && grantResults[0]
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    pickImageFromGalery()
+                } else {
+                    Toast.makeText(this, "Permissão Negada!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            PERMISSION_CODE_SHOOT_IMAGE -> {
+                if (grantResults.size > 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Permissão Negada!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+    private fun openCamera() {
+        TODO("Not yet implemented")
+    }
+
+    private fun pickImageFromGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            ivImage.setImageURI(data?.data)
+        }
     }
 }
